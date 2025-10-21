556bc5617998a36db05723798c6eea65ee06427f
Li Lin <guru.lin@gmail.com>
2025-10-21 13:18:02 +0000

    update container startup script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,7 @@
 
 cd "$(dirname "$0")"
 
-/bin/sleep 100000
+python manage.py collectstatic --no-input > /dev/null
+python manage.py migrate > /dev/null
+
+gunicorn --workers 1 --bind 0.0.0.0:8000 track_me.wsgi:application
